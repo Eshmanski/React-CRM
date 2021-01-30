69498692be12bce9e2e967dae4dfbcfffaf206cd
Eshmanski <Eshmanski@gmail.com>
2021-01-30 20:47:59 +0300

    save

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core';
+import { Button, InputLabel, Input, FormControl, TextField } from '@material-ui/core';
 import { LeadsContext } from '../../App.js';
 import { useContext, useState } from 'react';
 import s from './home.module.css';
@@ -35,14 +35,14 @@ function Home() {
             };
 
         addLead([...leads, lead]);
-        
+
         setFormProps({
             firstName: '',
             secondName: '',
             phone: '',
             description: '',
         });
-        
+
         event.preventDefault();
     };
 
@@ -50,22 +50,27 @@ function Home() {
         <div>
             <h2> Home page </h2>
             <form className={s.form} onSubmit={handleAddLead}>
-                <label>
-                    <div>Имя:</div>
-                    <input type='text' value={formProps.firstName} onChange={handleChange} name='firstName' />
-                </label>
-                <label>
-                    <div>Фамилия:</div>
-                    <input type='text' value={formProps.secondName} onChange={handleChange} name='secondName' />
-                </label>
-                <label>
-                    <div>Телефон:</div>
-                    <input type='text' value={formProps.phone} onChange={handleChange} name='phone' />
-                </label>
-                <label>
-                    <div>Описание:</div>
-                    <textarea value={formProps.description} onChange={handleChange} name='description'></textarea>
-                </label>
+                <FormControl>
+                    <InputLabel htmlFor='firstName'>Имя:</InputLabel>
+                    <Input id='firstName' type='text' value={formProps.firstName} onChange={handleChange} name='firstName' />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor='secondName'>Фамилия:</InputLabel>
+                    <Input id='secondName' type='text' value={formProps.secondName} onChange={handleChange} name='secondName' />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor='phone'>Телефон:</InputLabel>
+                    <Input id='phone' type='text' value={formProps.phone} onChange={handleChange} name='phone' />
+                </FormControl>
+                <TextField
+                    id="description"
+                    label="Описание заявки:"
+                    multiline
+                
+                    value={formProps.description}
+                    onChange={handleChange}
+                    name='description'
+                />
                 <Button variant='contained' type='submit'>Добавить</Button>
             </form>
         </div>
